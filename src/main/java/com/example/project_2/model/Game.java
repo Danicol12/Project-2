0576e456b07853f4732620ba70934a28b7ce3cb6
Daniel Andrade <andrade.daniel@correounivalle.edu.co>
2024-10-19 16:03:03 -0500

    Sudoku matrix created

--- src/main/java/com/example/project_2/model/Game.java
+++ src/main/java/com/example/project_2/model/Game.java
@@ -1,22 +1,45 @@
 package com.example.project_2.model;
 
+import java.util.ArrayList;
+
 public class Game {
 
-    private int[][] SudokuMatrix;
 
-    public Game(){
-        SudokuMatrix = new int[6][6];
+    private ArrayList<String>[] SudokuMatrix = new ArrayList[6];
+    private int[] auxNumbers = new int[6];
+
+    public Game() {
+        for (int i = 0; i < 6; i++) {
+            SudokuMatrix[i] = new ArrayList<>();
+        }
+        setSudokuMatrixValue();
+        this.SudokuMatrix[0].set(1, "1");
+
     }
-    public void showMatrix(int[][] matrix){
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] +"\t");
+
+
+    public void setSudokuMatrixValue() {
+
+        for (int i = 0; i < 6; i++) {
+            this.SudokuMatrix[i].clear();
+            for (int f = 0; f < 6; f++) {
+                this.SudokuMatrix[i].add("0");
             }
+
+        }
+    }
+
+
+    public void showMatrix() {
+        for (int i = 0; i < this.SudokuMatrix.length; i++) {
+
+            System.out.print(SudokuMatrix[i] + "\t");
+
             System.out.println();
         }
     }
-    public int[][] getSudokuMatrix(){
-        return SudokuMatrix;
-    }
+    public void fillAuxNumbers() {for(int i=0; i<6; i++){auxNumbers[i]=i;}}
 }
+
+
 
