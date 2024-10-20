c1d5657cac47b6abed90e33b0d083fd2640bd8e6
Daniel Andrade <andrade.daniel@correounivalle.edu.co>
2024-10-19 23:59:29 -0500

    The sugokus Matrix is correctly filled

---
+++ src/main/java/com/example/project_2/model/SudokuMatrix.java
@@ -0,0 +1,14 @@
+package com.example.project_2.model;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SugokuMatrix {
+
+    private ArrayList<String>[] SudokuMatrix = new ArrayList[6];
+    private int[] auxNumbers = new int[6];
+    private Random rand = new Random();
+    public SugokuMatrix() {
+        
+    }
+}
