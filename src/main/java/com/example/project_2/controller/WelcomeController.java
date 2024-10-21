97f237272c4c9be5baf6d38e171907186a848d04
Miguel Angel Castillo Sánchez <miguel.castillo.sanchez@correounivalle.edu.co>
2024-10-20 22:15:31 -0500

    Image has been put on the play button and the background of the welcomeStage has been adjusted

--- src/main/java/com/example/project_2/controller/WelcomeController.java
+++ src/main/java/com/example/project_2/controller/WelcomeController.java
@@ -2,28 +2,36 @@ package com.example.project_2.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.ImageInput;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 public class WelcomeController {
+
     @FXML
-    private Label welcomeText;
+    private Button playButton;
 
     @FXML
     private AnchorPane welcomePane;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private ImageView welcomeImageView;
 
     @FXML
 public void initialize() {
 
         Image welcomeImage = new Image(getClass().getResource("/com/example/project_2/images/welcome-bg.jpg").toExternalForm());
-        welcomePane.setEffect(new ImageInput(welcomeImage));
+        welcomeImageView.setImage(welcomeImage);
+
+        playButton.setEffect(new ImageInput(new Image(getClass().getResource("/com/example/project_2/images/play-button.png").toExternalForm())));
+
+//        Image playButtonPressed = new Image(getClass().getResource("/com/example/project_2/images/play-button.png").toExternalForm());
+//        playButton.setOnMousePressed(event -> {
+//            playButton.setEffect(new ImageInput(playButtonPressed));
+//        });
     }
 
     public void handlePlay(ActionEvent actionEvent) {
